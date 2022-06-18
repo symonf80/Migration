81fb46cd87cad920ac1e3191be31c7f13e25b06d
symonf80 <symonf80@mail.ru>
2022-06-18 17:09:47 +0300

    added files

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,46 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+import ru.netology.exception.NotFoundException;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class PostRepository {
+    private final ConcurrentMap<Long, Post> allPosts;
+    private final AtomicLong idCounter = new AtomicLong();
+
+    public PostRepository() {
+        this.allPosts = new ConcurrentHashMap<>();
+    }
+
+    public Collection<Post> all() {
+        return allPosts.values();
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(allPosts.get(id));
+    }
+
+    public Post save(Post savePost) {
+        if (savePost.getId() == 0) {
+            long id = idCounter.incrementAndGet();
+            savePost.setId(id);
+            allPosts.put(id, savePost);
+        } else if (allPosts.containsKey(savePost.getId())) {
+            allPosts.replace(savePost.getId(), savePost);
+        } else {
+            throw new NotFoundException();
+        }
+        return savePost;
+    }
+
+    public void removeById(long id) {
+        allPosts.remove(id);
+    }
+}
